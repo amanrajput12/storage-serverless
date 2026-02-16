7ab3aeb2b890fe027f287c63aa6c28d9a27de217
Aman Rajput <rajputaman724857@gmail.com>
2026-02-16 15:53:23 +0530

    fix  expiry error

--- config/cloudfront.js
+++ config/cloudfront.js
@@ -9,33 +9,26 @@ let distribution ='https://d19vnk21ika5bl.cloudfront.net'
 const keyPairId = "K3CE296GJ496XA";
 
 
-const dateLessThan =  new Date(Date.now() +1000 *60*60 ).toISOString(); // Expires in 1 hour
 
 
 
-export const createCloudfrontSignedUrl = async ({ key,download=false,filename }) => {
-  console.log("Creating Cloudfront signed URL with key:", key);
+export const createCloudfrontSignedUrl = async ({ key, download = false, filename }) => {
 
-let url = `${distribution.replace(/\/$/, "")}/${key}?response-content-disposition=${encodeURIComponent(
-  `
-  ${ download ? "attachment" : "inline"}; filename="${filename}"`
-)}`;
+  // Test for 5 minutes validity
+  const dateLessThan = new Date(Date.now() + 1000 * 60 * 5).toISOString();
+    console.log("time create ", dateLessThan);
+  const disposition = `${download ? "attachment" : "inline"}; filename="${filename}"`;
 
+  const url = `${distribution.replace(/\/$/, "")}/${key}?response-content-disposition=${encodeURIComponent(disposition)}`;
 
-
-// ?response-content-disposition=attachment;filename="test.png
-console.log("URL to sign:", url);
-
-
- 
-const signedurl = getSignedUrl({
+  const signedurl = getSignedUrl({
     url,
     keyPairId,
     privateKey: Privatekey,
     dateLessThan
-});
+  });
 
-console.log("Generated Cloudfront signed URL:", signedurl);
-return signedurl;
+  console.log("Generated signed URL inside cloudfront:", signedurl);
 
-}
+  return signedurl;
+};
